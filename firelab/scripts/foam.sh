5ac70e3053b8e1ba8f68032b5a4b8cb80d25ff64
Kyle Shannon <kyle@pobox.com>
2016-02-06 09:57:10 -0700

    use default path

--- firelab/scripts/foam.sh
+++ firelab/scripts/foam.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-git checkout patched
 export FOAM_INST_DIR="`dirname $PWD`";
 source $FOAM_INST_DIR/OpenFOAM-2.2.x/etc/bashrc \
        WM_OSTYPE=MSwindows \
@@ -17,6 +16,18 @@ cd $WM_PROJECT_DIR/wmake/src
 make
 ln -s $WM_PROJECT_DIR/wmake/platforms/linux64Gcc \
       $WM_PROJECT_DIR/wmake/platforms/linux64mingw-w64
+cd $WM_PROJECT_DIR
+source $FOAM_INST_DIR/OpenFOAM-2.2.x/etc/bashrc \
+       WM_OSTYPE=MSwindows \
+       WM_COMPILER=mingw-w64 \
+       WM_ARCH_OPTION=64 \
+       WM_PRECISION_OPTION=SP \
+       WM_CC=x86_64-w64-mingw32-gcc \
+       WM_CXX=x86_64-w64-mingw32-g++ \
+       compilerInstall=system \
+       WM_MPLIB=MSMPI \
+       MPI_ARCH_PATH=$FOAM_INST_DIR/OpenFOAM-2.2.x/firelab/msmpi
 ./Allwmake
-cd $WINDNINJA_HOME/src/ninjafoam/
+cd $HOME/src/windninja/src/ninjafoam/
 ./Allwmake
+

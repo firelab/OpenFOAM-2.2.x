9e17c48e760c47ce68ccd5a8d5918da0a94dc75e
Kyle Shannon <kyle@pobox.com>
2016-02-17 20:25:08 -0700

    only archive what we need.

--- firelab/scripts/foam.sh
+++ firelab/scripts/foam.sh
@@ -34,16 +34,89 @@ wmake libso
 cd $HOME/src/windninja/src/ninjafoam/utility/applyInit
 wmake
 cd $WM_PROJECT_DIR
-# copy some stuff into the dist
+
+# copy some stuff into the foam distribution folders.
 cp /usr/lib/gcc/x86_64-w64-mingw32/4.8/libgcc_s_sjlj-1.dll \
    platforms/linux64mingw-w64SPOpt/lib/
 cp /usr/x86_64-w64-mingw32/lib/libwinpthread-1.dll \
    platforms/linux64mingw-w64SPOpt/lib/
 cp /usr/lib/gcc/x86_64-w64-mingw32/4.8/libstdc++-6.dll \
    platforms/linux64mingw-w64SPOpt/lib/
+cp firelab/msmpi/Lib/amd64/msmpi64.dll platforms/linux64mingw-w64SPOpt/lib
 cp platforms/linux64mingw-w64SPOpt/lib/msmpi/*.dll platforms/linux64mingw-w64SPOpt/lib
 cp $FOAM_USER_LIBBIN/*.dll platforms/linux64mingw-w64SPOpt/lib/
 cp $FOAM_USER_APPBIN/*.exe platforms/linux64mingw-w64SPOpt/bin/
+
+# zip etc
 zip -r $PWD.zip etc/ \
-                platforms/linux64mingw-w64SPOpt/lib/*.dll \
-                platforms/linux64mingw-w64SPOpt/bin/*.exe
+
+# zip all of the none-scotch dependant executables
+for fname in "surfaceTransformPoints" \
+             "surfaceCheck" \
+             "moveDynamicMesh" \
+             "topoSet" \
+             "refineMesh" \
+             "blockMesh" \
+             "surfaceTransformPoints" \
+             "surfaceCheck" \
+             "checkMesh" \
+             "applyInit" \
+             "simpleFoam" \
+             "sample" \
+             "applyInit";
+do
+    zip $PWD.zip platforms/linux64mingw-w64SPOpt/bin/${fname}.exe
+done
+
+# uncomment if we have scotch
+#for fname in "decomposePar" \
+#             "reconstructParMesh" \
+#             "reconstructPar" \
+#             "renumberMesh";
+#do
+#    zip $PWD.zip platforms/linux64mingw-w64SPOpt/bin/${fname}.exe
+#donelibincompressibleRASModels
+
+# zip the dlls that the executables depend on.  These were found using
+# dependancy walker and trial and error.
+for fname in "libOpenFOAM" \
+             "libfiniteVolume" \
+             "libgcc_s_sjlj-1" \
+             "libstdc++-6" \
+             "libmeshTools" \
+             "libtriSurface" \
+             "libdynamicFvMesh" \
+             "libsampling" \
+             "libPstream" \
+             "libdynamicMesh" \
+             "libblockMesh" \
+             "libsurfMesh" \
+             "libfvOptions" \
+             "libincompressibleRASModels" \
+             "libincompressibleTransportModels" \
+             "libincompressibleTurbulenceModel" \
+             "libgenericPatchFields" \
+             "libwinpthread-1" \
+             "libfileFormats" \
+             "libextrudeModel" \
+             "libconversion" \
+             "liblagrangian" \
+             "libcompressibleTurbulenceModel" \
+             "libfluidThermophysicalModels" \
+             "libspecie" \
+             "libconversion" \
+             "libsolidThermo" \
+             "libreactionThermophysicalModels" \
+             "libtwoPhaseMixture" \
+             "libsolidSpecie" \
+             "libfvMotionSolvers" \
+             "msmpi64" \
+             "libWindNinja";
+do
+    zip $PWD.zip platforms/linux64mingw-w64SPOpt/lib/${fname}.dll
+done
+
+# just get everything
+#zip -r $PWD.zip etc/ \
+#                platforms/linux64mingw-w64SPOpt/lib/*.dll \
+#                platforms/linux64mingw-w64SPOpt/bin/*.exe

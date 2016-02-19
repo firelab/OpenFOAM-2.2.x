2c53dc38fb702743d4bb29356502af25feca727e
Kyle Shannon <kyle@pobox.com>
2016-02-18 20:16:36 -0700

    fail early, get mpiexec and zlib dlls

--- firelab/scripts/foam.sh
+++ firelab/scripts/foam.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 export FOAM_INST_DIR="`dirname $PWD`";
-source $FOAM_INST_DIR/OpenFOAM-2.2.x/etc/bashrc \
-       WM_OSTYPE=MSwindows \
-       WM_COMPILER=mingw-w64 \
-       WM_ARCH_OPTION=64 \
-       WM_PRECISION_OPTION=SP \
-       WM_CC=x86_64-w64-mingw32-gcc \
-       WM_CXX=x86_64-w64-mingw32-g++ \
-       compilerInstall=system \
-       WM_MPLIB=MSMPI \
-       MPI_ARCH_PATH=$FOAM_INST_DIR/OpenFOAM-2.2.x/firelab/msmpi
 
 source $FOAM_INST_DIR/OpenFOAM-2.2.x/etc/bashrc
 cd $WM_PROJECT_DIR/wmake/src
 make
 ln -s $WM_PROJECT_DIR/wmake/platforms/linux64Gcc \
       $WM_PROJECT_DIR/wmake/platforms/linux64mingw-w64
+
 source $FOAM_INST_DIR/OpenFOAM-2.2.x/etc/bashrc \
        WM_OSTYPE=MSwindows \
        WM_COMPILER=mingw-w64 \
@@ -27,28 +18,31 @@ source $FOAM_INST_DIR/OpenFOAM-2.2.x/etc/bashrc \
        WM_MPLIB=MSMPI \
        MPI_ARCH_PATH=$FOAM_INST_DIR/OpenFOAM-2.2.x/firelab/msmpi
 
-cd $WM_PROJECT_DIR
-./Allwmake
-cd $HOME/src/windninja/src/ninjafoam/
-wmake libso
-cd $HOME/src/windninja/src/ninjafoam/utility/applyInit
-wmake
-cd $WM_PROJECT_DIR
+cd $WM_PROJECT_DIR &&
+find . -name Allwmake -exec chmod +x '{}' \;
+./Allwmake &&
+cd $HOME/src/windninja/src/ninjafoam/ &&
+wmake libso &&
+cd $HOME/src/windninja/src/ninjafoam/utility/applyInit &&
+wmake &&
+cd $WM_PROJECT_DIR &&
 
 # copy some stuff into the foam distribution folders.
 cp /usr/lib/gcc/x86_64-w64-mingw32/4.8/libgcc_s_sjlj-1.dll \
-   platforms/linux64mingw-w64SPOpt/lib/
+   platforms/linux64mingw-w64SPOpt/lib/ &&
 cp /usr/x86_64-w64-mingw32/lib/libwinpthread-1.dll \
-   platforms/linux64mingw-w64SPOpt/lib/
+   platforms/linux64mingw-w64SPOpt/lib/ &&
 cp /usr/lib/gcc/x86_64-w64-mingw32/4.8/libstdc++-6.dll \
-   platforms/linux64mingw-w64SPOpt/lib/
-cp firelab/msmpi/Lib/amd64/msmpi64.dll platforms/linux64mingw-w64SPOpt/lib
-cp platforms/linux64mingw-w64SPOpt/lib/msmpi/*.dll platforms/linux64mingw-w64SPOpt/lib
-cp $FOAM_USER_LIBBIN/*.dll platforms/linux64mingw-w64SPOpt/lib/
-cp $FOAM_USER_APPBIN/*.exe platforms/linux64mingw-w64SPOpt/bin/
+   platforms/linux64mingw-w64SPOpt/lib/ &&
+cp firelab/msmpi/Lib/amd64/msmpi64.dll platforms/linux64mingw-w64SPOpt/lib &&
+cp firelab/msmpi/Bin/mpiexec.exe platforms/linux64mingw-w64SPOpt/bin &&
+cp firelab/zlib-1.2.8/zlib1.dll platforms/linux64mingw-w64SPOpt/lib &&
+cp platforms/linux64mingw-w64SPOpt/lib/msmpi/*.dll platforms/linux64mingw-w64SPOpt/lib &&
+cp $FOAM_USER_LIBBIN/*.dll platforms/linux64mingw-w64SPOpt/lib/ &&
+cp $FOAM_USER_APPBIN/*.exe platforms/linux64mingw-w64SPOpt/bin/ &&
 
 # zip etc
-zip -r $PWD.zip etc/
+zip -r $PWD.zip etc/ &&
 
 # zip all of the none-scotch dependant executables
 for fname in "surfaceTransformPoints" \
@@ -63,9 +57,10 @@ for fname in "surfaceTransformPoints" \
              "applyInit" \
              "simpleFoam" \
              "sample" \
-             "applyInit";
+             "applyInit" \
+             "mpiexec";
 do
-    zip $PWD.zip platforms/linux64mingw-w64SPOpt/bin/${fname}.exe
+    zip $PWD.zip platforms/linux64mingw-w64SPOpt/bin/${fname}.exe &&
 done
 
 # uncomment if we have scotch
@@ -83,6 +78,7 @@ for fname in "libOpenFOAM" \
              "libfiniteVolume" \
              "libgcc_s_sjlj-1" \
              "libstdc++-6" \
+             "zlib1.dll" \
              "libmeshTools" \
              "libtriSurface" \
              "libdynamicFvMesh" \
@@ -113,7 +109,7 @@ for fname in "libOpenFOAM" \
              "msmpi64" \
              "libWindNinja";
 do
-    zip $PWD.zip platforms/linux64mingw-w64SPOpt/lib/${fname}.dll
+    zip $PWD.zip platforms/linux64mingw-w64SPOpt/lib/${fname}.dll &&
 done
 
 # just get everything
